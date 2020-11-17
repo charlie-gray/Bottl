ad1e9cf19470462e65f95cd5d21c4a9c6061bce2
Charlie Gray <charliegray.dev@gmail.com>
2020-11-17 11:49:00 -0600

    Added cocoa pods + UI

---
+++ Bottl/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Bottl
+//
+//  Created by charlie on 11/11/20.
+//
+
+import UIKit
+import Firebase
+class ViewController: UIViewController {
+
+    @IBOutlet weak var MessageOutText: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func RefreshMessage(_ sender: Any) {
+    }
+    
+}
+
