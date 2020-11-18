46adde9ffceac5db6f83a5a2301e737f26fc1d1f
Charlie Gray <charliegray.dev@gmail.com>
2020-11-18 14:34:27 -0600

    Updated UI Allowed for Modal closure Added function to get messages

--- Bottl/ViewController.swift
+++ Bottl/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 import Firebase
 class ViewController: UIViewController {
-
+    let db = Firestore.firestore()
+    
+    @IBOutlet weak var msgOut: UILabel!
+    
+    @IBOutlet weak var dateTimeOut: UILabel!
+    
+    @IBOutlet var ScreenA: UIView!
+    
     @IBOutlet weak var MessageOutText: UILabel!
     
     override func viewDidLoad() {
@@ -18,6 +25,57 @@ class ViewController: UIViewController {
 
     @IBAction func RefreshMessage(_ sender: Any) {
     }
+    
+    @IBAction func returnHome(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+    func betterPrintGenericInfo<T>(_ value: T) {
+        let t = type(of: value as Any)
+        print("'\(value)' of type '\(t)'")
+    }
+
+
+   
+    
+    
+    @IBAction func getMessage(_ sender: Any){
+
+            db.collection("messages").getDocuments() { (querySnapshot, err) in
+                    if let err = err {
+                        print("Error getting documents: \(err)")
+                    }
+                    else{
+                        let data = self.duplicateRemover(querySnapshot: querySnapshot!)
+
+                        self.updateMessageOutputs(data: data)
+                    }
+        }
+    }
+    
+    
+    func duplicateRemover(querySnapshot : QuerySnapshot) -> QueryDocumentSnapshot{
+        
+        var data = querySnapshot.documents.randomElement()!
+        while (!self.isDuplicate(data: data)){
+            data = querySnapshot.documents.randomElement()!
+        }
+        
+        return data
+    }
+    
+    func isDuplicate(data : QueryDocumentSnapshot) -> Bool {
+        return ((data.get("content") as? String) != msgOut.text)
+    }
+    
+    
+    func updateMessageOutputs(data : QueryDocumentSnapshot){
+        self.msgOut.text = data.get("content") as? String
+        self.dateTimeOut.text = (data.get("date") as? String ?? "Unknown Date") + " - " + (data.get("time") as? String ?? "Unknown Time")
+
+    }
+    
+    
     
 }
 
